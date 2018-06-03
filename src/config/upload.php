105ef9243b549baf45749dbea2eed1daa00128fd
baymax <baymaxrice@gmail.com>
2018-06-03 14:23:15 +0800

    fix

--- src/config/upload.php
+++ src/config/upload.php
@@ -10,4 +10,23 @@ return [
         'middleware'    => ['web'],
 
     ],
+
+    // upload file configration
+    'upload'  => [
+
+        'enable' => true,
+
+        'disk' => 'public',
+
+        'directory'  => [
+            'image'  => 'images',
+            'file'   => 'files',
+        ],
+
+        // file size, unit: M
+        'limit' => [
+            'image' => 10,
+            'file' => 10,
+        ]
+    ],
 ];
